7af6f40a8d0e7260e05fa179a0b767116e99b3a1
alice-poole <ap3789@drexel.edu>
2023-03-07 06:03:35 -0500

    bug catch

--- wp-content/themes/bloomblog/components/404.php
+++ wp-content/themes/bloomblog/components/404.php
@@ -7,14 +7,3 @@
         </div>
     </div>
 </section>
-
-<div class="content">
-        <p>Hero Home</p>
-        <div class="content-outside">
-            <h1><?php echo get_bloginfo('name'); ?></h1>
-            <h3><!--Creator Name - Note - Replace with Custom Field-->Alice Poole</h3>
-        </div>
-        <div class="featured-image arch narrow outline">
-        </div>
-    </div>
-</section>
