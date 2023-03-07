006fe61328aa61b94564250c3ecf3510e4d66274
alice-poole <ap3789@drexel.edu>
2023-03-07 14:24:05 -0500

    Update front-page.php

--- wp-content/themes/bloomblog/front-page.php
+++ wp-content/themes/bloomblog/front-page.php
@@ -2,6 +2,6 @@
 
 <?php get_template_part('components/hero/home'); ?>
 <?php get_template_part('components/list/pages'); ?>
-<?php get_template_part('components/about'); ?>
+<?php get_template_part('components/content'); ?>
 
 <?php get_footer('outside'); ?>
