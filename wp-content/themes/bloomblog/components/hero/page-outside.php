709824ced3e0c6f36834ce4b6ae7b98caf782568
alice-poole <ap3789@drexel.edu>
2023-03-14 04:05:45 -0400

    Cleaned up old files

--- wp-content/themes/bloomblog/components/hero/page-outside.php
+++ wp-content/themes/bloomblog/components/hero/page-outside.php
@@ -1,6 +1,6 @@
 <div class="content">
         <div class="content-outside">
-            <h1 class="color"><?php echo get_the_title(); ?></h1>
+            <h1><?php echo get_the_title(); ?></h1>
             <!--<h4>Categories or custom subtitle? - TODO: Replace with Custom Field</h4>-->
         </div>
         <br>
