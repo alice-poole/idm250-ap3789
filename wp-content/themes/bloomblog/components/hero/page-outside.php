7f9023b92f27f502010224edd002fa2928685c6e
alice-poole <ap3789@drexel.edu>
2023-03-14 03:17:20 -0400

    spacing fixes

--- wp-content/themes/bloomblog/components/hero/page-outside.php
+++ wp-content/themes/bloomblog/components/hero/page-outside.php
@@ -3,6 +3,7 @@
             <h1 class="color"><?php echo get_the_title(); ?></h1>
             <!--<h4>Categories or custom subtitle? - TODO: Replace with Custom Field</h4>-->
         </div>
+        <br>
         <div class="featured-image arch narrow outline">
         </div>
     </div>
--- wp-content/themes/bloomblog/components/hero/post.php
+++ wp-content/themes/bloomblog/components/hero/post.php
@@ -1,6 +1,7 @@
 <div class="content">
         <div class="featured-image rectangle wide outline">
         </div>
+        <br>
         <div>
             <h1><?php echo get_the_title(); ?></h1>
             <!--<h4>Categories or custom subtitle? - TODO: Replace with Custom Field</h4>-->
